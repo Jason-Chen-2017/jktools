a73f1ec7197dbef386bb303d4ac0a2a1ffa5215a
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-06-14 03:50:27 +0800

    0614

--- kill_process.sh
+++ kill_process.sh
@@ -2,3 +2,5 @@
 ps -ef|grep chrome|awk '{print $2}'| xargs kill -9
 #批量 kill全部java进程
 ps -ef|grep java|awk '{print $2}'| xargs kill -9
+#批量 kill全部intellij-idea-ultimate进程
+ps -ef|grep intellij-idea-ultimate|awk '{print $2}'| xargs kill -9

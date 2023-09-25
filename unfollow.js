3184bb8aba6729d4c4f616837b0e39ba8e8afd7a
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-09-25 11:20:22 +0800

    var arr = document.getElementsByClassName('subed-btn'); for (let index = 0; index < arr.length; index++) {     arr[index].click(); }

--- unfollow.js
+++ unfollow.js
@@ -2,3 +2,8 @@ var items = document.getElementsByClassName('subed-btn');
 for (let index = items.length - 1; index > items.length - 100; index--) {
     items[index].click();
 }
+
+var arr = document.getElementsByClassName('subed-btn');
+for (let index = 0; index < arr.length; index++) {
+    arr[index].click();
+}
